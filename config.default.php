56214a2116f77a433b3dd20ad193215e351721f9
oyps <ponconsoft@gmail.com>
2022-08-27 13:20:51 +0800

    优化

---
+++ config.default.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 配置文件
+ */
+$config = array(
+    'mysql' => array(
+        'host' => '127.0.0.1',
+        'user' => 'root',
+        'pass' => '',
+        'db' => ''
+    ),
+    'table' => array(
+        'url' => 'short_url_url'
+    ),
+    'admin' => array(
+        'password' => '' // 管理员密码
+    )
+);
+date_default_timezone_set('Asia/Shanghai');
